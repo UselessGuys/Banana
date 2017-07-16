8f45e7e2ccec9577034bfb391a1fafd6f3ffddd3
alexdanili <adm.minefun@yandex.ru>
2017-07-16 17:41:51 +0300

    Start writing Player movement script

---
+++ Assets/Scripts/PlayerTrigger.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTrigger : MonoBehaviour {
+
+    public bool Triggered = false;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D obj)
+    {
+        Destroy(gameObject);
+        Destroy(obj.gameObject);
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Triggered = false;
+    }
+}
