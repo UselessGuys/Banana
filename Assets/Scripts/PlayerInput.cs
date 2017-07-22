85a056edb47d551748fad94e1c566fa16dae13a7
alexdanili <adm.minefun@yandex.ru>
2017-07-22 19:46:26 +0300

    added based animations

--- Assets/Scripts/PlayerInput.cs
+++ Assets/Scripts/PlayerInput.cs
@@ -4,10 +4,15 @@
 public class PlayerInput : MonoBehaviour
 {
     private Player player;
-
+    Animator anim;
+    private Controller2D controller;
+    private SpriteRenderer rend;
     private void Start()
     {
         player = GetComponent<Player>();
+        rend = GetComponent<SpriteRenderer>();
+        controller = GetComponent<Controller2D>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
@@ -15,10 +20,22 @@ public class PlayerInput : MonoBehaviour
         Vector2 directionalInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         player.SetDirectionalInput(directionalInput);
 
+        if(Input.GetAxisRaw("Horizontal") != 0 && controller.collisions.below)
+        {
+            anim.SetBool("PlayerMove", true);
+        }
+        else
+        {
+            anim.SetBool("PlayerMove", false);
+        }
+        anim.SetBool("Grounded", controller.collisions.below);
+
         if (Input.GetButtonDown("Jump"))
         {
             player.OnJumpInputDown();
         }
+
+        rend.flipX = (Input.GetAxisRaw("Horizontal") < 0);
 
         if (Input.GetButtonUp("Jump"))
         {
