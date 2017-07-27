2bfb7d3b5d140e1b0490cf1c5a343cc799c68938
AndrewZhirkov <AndrewDoge@mail.ru>
2017-07-27 22:19:29 +0300

    Добавлено движение патрулирующего врага

--- Assets/Scripts/CharacterStats.cs
+++ Assets/Scripts/CharacterStats.cs
@@ -2,16 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 
 namespace Assets.Scripts
 {
-    class CharacterStats
+    class CharacterStats : MonoBehaviour
     {
         public int Health;
         public int Mana;
         public int Rage;
 
-        public int MoveSpeed;
+        public float MoveSpeed;
         public int JumpHeight;
 
         public int AttackSpeed;
